e89d333fbc82025c849a81a117ecb4d518d674eb
renaud <rno.bstin@gmail.com>
2018-09-26 13:35:19 +0200

    started hdr

--- timeLapser.py
+++ timeLapser.py
@@ -54,7 +54,7 @@ def dbFiller():
                         if image is not None:
                             images.append(image)
                             times.append(timers[len(images)-1])
-                    time = np.array(times)
+                    times = np.array(times)
                         
 
                     alignMTB = cv2.createAlignMTB()
