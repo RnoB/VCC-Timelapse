3df0e53d6bf2ba0caf135dcb87360a6ac0454d93
renaud <rno.bstin@gmail.com>
2018-09-26 12:20:32 +0200

    started hdr

--- timeLapser.py
+++ timeLapser.py
@@ -50,6 +50,8 @@ def dbFiller():
                     for ev in evs:
                         imName = imagePath+year+'-'+month+'-'+day+'_'+hours+minutes+ev+'.jpg'
                         images.append(cv2.imread(imName))
+                    print(images)
+                    print(image[0])
                     alignMTB = cv2.createAlignMTB()
                     alignMTB.process(images, images)
                     calibrateDebevec = cv2.createCalibrateDebevec()
