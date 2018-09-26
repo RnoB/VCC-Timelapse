daee0b9447bbac24eb3ba00532f52cb8a5088309
renaud <rno.bstin@gmail.com>
2018-09-26 10:46:22 +0200

    check files date

---
+++ timeLapser.py
@@ -0,0 +1,24 @@
+import cv2
+import sqlite3
+import numpy as np
+import os
+
+imagePath = '/timelapse'
+
+
+
+def dbFiller():
+    files = os.listdirs(imagePath)
+    fileDate = []
+    for file  in files:
+        fileDate.append(file[0:15])
+    fileDate = np.unique(fileDate)
+    print(fileDate)
+
+
+def main():
+    dbFiller()
+    print('nothing')
+
+if __name__ == '__main__':
+    main()
