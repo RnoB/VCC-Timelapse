7f8bfca62584c40a223a947829bea146342df104
renaud <rno.bstin@gmail.com>
2018-09-27 11:20:52 +0200

    debug video creation

--- timeLapser.py
+++ timeLapser.py
@@ -43,7 +43,7 @@ def firstGenDb():
 def pather(path,expId):
     if not os.path.exists(path):
         os.makedirs(path)
-    path = path  + expId+ '\\'
+    path = path  + expId+ '/'
     if not os.path.exists(path):
         os.makedirs(path)
     
