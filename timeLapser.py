3ba385e7d7ff0adfb23860cdd95d4f4287b1e0d3
renaud <rno.bstin@gmail.com>
2018-09-28 14:02:06 +0200

    correct frameRate

--- timeLapser.py
+++ timeLapser.py
@@ -195,12 +195,13 @@ def weeklyVideo():
                 videoName = 'week'+str(week).zfill(5)+'.mp4'
                 videoLine = ffmpegWeek + weekTemp+videoName
                 print(videoLine)
-                subprocess.call(videoLine,shell=True)
+                #subprocess.call(videoLine,shell=True)
                 copyfile(path,pather(weekVid,str(week).zfill(5))+videoName)
+                print(path)
                 videoId = upload_video(path,title = "Week "+str(week))
-                values = [videoId,"week",year,month,dayRec,int(week)]
+                values = [videoId,"week",year,month,day,int(week)]
 
-                c.execute("INSERT INTO images VALUES (?,?,?,?,?,?)",values)
+                #c.execute("INSERT INTO images VALUES (?,?,?,?,?,?)",values)
                 print(year+' '+month+' '+day+' '+hours+':'+minutes + ' week : '+str(week) + ' day : '+str(weekday) +' dayRec : '+str(dayRec))
             
                 conn.commit()
